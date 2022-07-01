355a59885857e75550221cbf1f5fc0ea5a78cb73
Roman Babiy <romchik.babiy@gmail.com>
2022-07-01 16:45:05 +0300

    upd

--- src/js/fetchRequest.js
+++ src/js/fetchRequest.js
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const API_KEY = '27830209-3f5c1f8822da29de83683a02e';
+const API_KEY = '28384952-cb198b155a0b3b70fc06988d8';
 const BASE_LINK = 'https://pixabay.com/api/';
 
-export const fetchImgsData = async (data, page) => {
-  const searchParams = new URLSearchParams({
-    key: API_KEY,
-    q: data,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page: page,
-  });
+export const fetchImgsData = async(data, page) => {
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: data,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        per_page: 40,
+        page: page,
+    });
 
-  return (await axios.get(`${BASE_LINK}?${searchParams}`)).data;
+    return (await axios.get(`${BASE_LINK}?${searchParams}`)).data;
 };
